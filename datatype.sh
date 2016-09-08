b8c0e07c737cb2532ce82dc3a23c0140a0143439
kamalkishorsingh3 <kamalkishorsingh3@gmail.com>
2016-09-08 15:29:29 +0530

    Datatype Check

--- datatype.sh
+++ datatype.sh
@@ -3,6 +3,6 @@ echo "Enter Your Number"
 read num
 if [ "$num" -ge 0 ]
         then
-            echo "The value integers"
+            echo "your value is integers"
 else "Your value is string"
 fi
