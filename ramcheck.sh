2b1d82fc77bafca6bf35a177273a227d63d1d119
aryaneeraj88 <aryaneeraj01@gmail.com>
2016-09-09 18:23:32 +0530

    removed blank line

--- ramcheck.sh
+++ ramcheck.sh
@@ -1,11 +1,5 @@
-
 total_memory=$(free -m | awk '{print $2}' | head -2 | tail -1);
-
 free_memory=$(free -m | awk '{print $4}'| head -3 | tail -1);
-
 let a="$free_memory * 100"
-
 let b="$a / $total_memory"
-
 echo "free ram is $b%";
-
